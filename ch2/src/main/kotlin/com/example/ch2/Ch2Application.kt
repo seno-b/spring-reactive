02824594b6f88301c753d820583ca8cd2a5d0f73
shinho_bang <shinho_bang@kolon.com>
2023-01-18 14:44:45 +0900

    init ch2

---
+++ ch2/src/main/kotlin/com/example/ch2/Ch2Application.kt
@@ -0,0 +1,11 @@
+package com.example.ch2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Ch2Application
+
+fun main(args: Array<String>) {
+    runApplication<Ch2Application>(*args)
+}
