f996309d0d246b678e6706c6fb378ece89b28f06
seno-b <bangshinho@gmail.com>
2023-01-18 17:58:49 +0900

    feat: add variables

--- ch2/src/main/kotlin/com/example/ch2/model/Item.kt
+++ ch2/src/main/kotlin/com/example/ch2/model/Item.kt
@@ -1,10 +1,19 @@
 package com.example.ch2.model
 
 import org.springframework.data.annotation.Id
+import java.awt.Point
+import java.util.*
 
 class Item(
     @Id
     var id: String? = null,
     var name: String,
     var price: Double
-)
+) {
+    private lateinit var description: String
+    private lateinit var distributorRegion: String
+    private lateinit var releaseDate: Date
+    private var availableUnits: Int = 0
+    private lateinit var location: Point
+    private var active: Boolean = false
+}
