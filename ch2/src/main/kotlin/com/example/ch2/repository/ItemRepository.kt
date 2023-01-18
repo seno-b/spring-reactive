22bdb626b2abbd7f3b8d0b1c9fa87498f6504687
seno-b <bangshinho@gmail.com>
2023-01-18 17:59:04 +0900

    feat: add find by name containing method

--- ch2/src/main/kotlin/com/example/ch2/repository/ItemRepository.kt
+++ ch2/src/main/kotlin/com/example/ch2/repository/ItemRepository.kt
@@ -2,6 +2,8 @@ package com.example.ch2.repository
 
 import com.example.ch2.model.Item
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
+import reactor.core.publisher.Flux
 
 interface ItemRepository: ReactiveCrudRepository<Item, String> {
+    fun findByNameContaining(partialName: String): Flux<Item>
 }
