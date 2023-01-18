fc318723a5c2fe7ffa3ee8b542d0cdfa826c6bb3
seno-b <bangshinho@gmail.com>
2023-01-18 16:28:06 +0900

    feat: create home api

--- ch2/src/main/kotlin/com/example/ch2/controller/HomeController.kt
+++ ch2/src/main/kotlin/com/example/ch2/controller/HomeController.kt
@@ -1,16 +1,29 @@
 package com.example.ch2.controller
 
+import com.example.ch2.model.Cart
 import com.example.ch2.repository.CartRepository
 import com.example.ch2.repository.ItemRepository
-import com.example.ch2.service.CartService
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.reactive.result.view.Rendering
+import reactor.core.publisher.Mono
 
 
 @Controller
 class HomeController(
     private val itemRepository: ItemRepository,
     private val cartRepository: CartRepository,
-    private val cartService: CartService
 ) {
 
+    @GetMapping("/home")
+    fun home(): Mono<Rendering> {
+        // 타임 리프도 리액티브 스트림 호환 템플릿 엔진이다.
+        return Mono.just(
+            Rendering.view("home.html")
+                .modelAttribute("items", this.itemRepository.findAll())
+                .modelAttribute("cart", this.cartRepository.findById("My Cart")
+                    .defaultIfEmpty(Cart("My Cart"))
+            ).build()
+        )
+    }
 }
--- ch2/src/main/kotlin/com/example/ch2/model/Item.kt
+++ ch2/src/main/kotlin/com/example/ch2/model/Item.kt
@@ -1,17 +1,10 @@
 package com.example.ch2.model
 
 import org.springframework.data.annotation.Id
-import java.util.*
 
-data class Item constructor(
+class Item(
     @Id
-    val id: String,
-    val name: String,
-    val price: Double
-) {
-    private val description: String? = null
-    private val distributorRegion: String? = null
-    private val releaseDate: Date? = null
-    private val availableUnits = 0
-    private val active = false
-}
+    var id: String? = null,
+    var name: String,
+    var price: Double
+)
