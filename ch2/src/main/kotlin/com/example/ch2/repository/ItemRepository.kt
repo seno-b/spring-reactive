2b42fd66a3d0ee7ccb7a998bbf28fcca8ee6e717
seno-b <bangshinho@gmail.com>
2023-01-27 16:16:22 +0900

    feat: extends reactive query by example executor

--- ch2/src/main/kotlin/com/example/ch2/repository/ItemRepository.kt
+++ ch2/src/main/kotlin/com/example/ch2/repository/ItemRepository.kt
@@ -2,10 +2,11 @@ package com.example.ch2.repository
 
 import com.example.ch2.model.Item
 import org.springframework.data.mongodb.repository.Query
+import org.springframework.data.repository.query.ReactiveQueryByExampleExecutor
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import reactor.core.publisher.Flux
 
-interface ItemRepository: ReactiveCrudRepository<Item, String> {
+interface ItemRepository: ReactiveCrudRepository<Item, String>, ReactiveQueryByExampleExecutor<Item> {
     // query method 방식
     fun findByNameContaining(partialName: String): Flux<Item>
 
