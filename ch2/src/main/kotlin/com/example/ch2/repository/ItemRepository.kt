43820ef5688673febf7acf87d902f555e55cba9a
shinho_bang <shinho_bang@kolon.com>
2023-01-27 15:38:58 +0900

    feat: query annotation method

--- ch2/src/main/kotlin/com/example/ch2/repository/ItemRepository.kt
+++ ch2/src/main/kotlin/com/example/ch2/repository/ItemRepository.kt
@@ -1,9 +1,18 @@
 package com.example.ch2.repository
 
 import com.example.ch2.model.Item
+import org.springframework.data.mongodb.repository.Query
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import reactor.core.publisher.Flux
 
 interface ItemRepository: ReactiveCrudRepository<Item, String> {
+    // query method 방식
     fun findByNameContaining(partialName: String): Flux<Item>
+
+    // 사용자 정의 query 방식
+    @Query("{ 'name' :  ?0, 'age' :  ?1 }")
+    fun findItemsForCustomerMonthlyReport(name: String, age: Int): Flux<Item>
+
+    @Query(sort = "{ 'age' :  -1 }")
+    fun findSortedStuffForWeeklyReport(): Flux<Item>
 }
